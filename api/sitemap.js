a543ec0891d8f1eed56b65e958a7d55097f6ed3a
Filip Vrba <filipvrba@pm.me>
2025-03-03 20:54:38 +0100

    add dynamic sitemap

---
+++ api/sitemap.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  let baseUrl = "https://filipvrba-blog.vercel.app";
+  let articles = await getArticleIds(baseUrl);
+
+  let urls = articles.map(a => (
+    `<url><loc>${baseUrl}/article/${a.id}</loc></url>`
+  )).join("");
+
+  let sitemap = `<?xml version='1.0' encoding='UTF-8'?>
+  <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
+      <url><loc>${baseUrl}</loc></url>
+      ${urls}
+  </urlset>`;
+  res.setHeader("Content-Type", "text/xml");
+  return res.status(200).send(sitemap)
+};
+
+async function getArticleIds(baseUrl) {
+  let query = "SELECT id FROM articles WHERE is_published = 1 ORDER BY created_at DESC;";
+  let queryEncode = encodeURIComponent(query);
+  let apiUrl = `${baseUrl}/api/bef?query=${queryEncode}`;
+  let response = await fetch(apiUrl);
+  if (!response.ok) return [];
+  let articles = await response.json();
+  return articles
+}
---
+++ src/root/api/sitemap.js.rb
@@ -0,0 +1,29 @@
+export default async def handler(req, res)
+  base_url = 'https://filipvrba-blog.vercel.app'
+  articles = await get_article_ids(base_url)
+  urls     = articles.map {|a| "<url><loc>#{base_url}/article/#{a.id}</loc></url>"}.join('')
+
+  sitemap  = "<?xml version='1.0' encoding='UTF-8'?>
+  <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
+      <url><loc>#{base_url}</loc></url>
+      #{urls}
+  </urlset>"
+
+  res.set_header('Content-Type', 'text/xml')
+  res.status(200).send(sitemap)
+end
+
+async def get_article_ids(base_url)
+  query        = "SELECT id FROM articles WHERE is_published = 1 ORDER BY created_at DESC;"
+  query_encode = encodeURI_component(query)
+  api_url      = "#{base_url}/api/bef?query=#{query_encode}"
+  response     = await fetch(api_url)
+
+  unless response.ok
+    return []
+  end
+  
+  articles = await response.json()
+
+  return articles
+end
