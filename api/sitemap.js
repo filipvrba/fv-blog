a19ffc9139e8bd2a9e79cd2e7430e80cd2227d74
Filip Vrba <filipvrba@pm.me>
2025-03-03 22:14:07 +0100

    change sitemap date

--- api/sitemap.js
+++ api/sitemap.js
@@ -8,7 +8,10 @@ export default async function handler(req, res) {
 
   let sitemap = `<?xml version='1.0' encoding='UTF-8'?>
   <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
-    <url><loc>${baseUrl}</loc></url>
+    <url>
+      <loc>${baseUrl}</loc>
+      <lastmod>2025-02-28</lastmod>
+    </url>
     ${urls}
   </urlset>`;
   res.setHeader("Content-Type", "text/xml");
