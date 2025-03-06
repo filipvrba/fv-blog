579f0ea2b5bde62dccdf6a8405d17725c4ac36aa
Filip Vrba <filipvrba@pm.me>
2025-03-06 12:26:17 +0100

    add yt rule

--- src/js/core/md_rules.js
+++ src/js/core/md_rules.js
@@ -11,13 +11,33 @@ export default class MDRules {
     return true
   };
 
+  static ytLoader(state, silent) {
+    let pos = state.pos;
+    let src = state.src;
+    let match = src.slice(pos).match(/^yt\[(https:\/\/www\.youtube\.com\/embed\/[a-zA-Z0-9_-]+[^\]]*)\]/m);
+    if (!match) return false;
+    if (silent) return false;
+    let token = state.push("yt_loader", "", 0);
+    token.attrs = {uri: match[1]};
+    state.pos += match[0].length;
+    return true
+  };
+
   static register(md) {
     md.inline.ruler.before("text", "img_loader", MDRules.imgLoader);
 
-    return md.renderer.rules.img_loader = (tokens, idx, options, env, selfObj) => {
+    md.renderer.rules.img_loader = (tokens, idx) => {
       let token = tokens[idx];
       let fileId = token.attrs.fileId;
       return `${`<div class='card anim-card border-0 bg-transparent'>\n        <elm-img-loader rounded file-id='${fileId}'></elm-img-loader>\n      </div>`}`
+    };
+
+    md.inline.ruler.before("text", "yt_loader", MDRules.ytLoader);
+
+    return md.renderer.rules.yt_loader = (tokens, idx) => {
+      let token = tokens[idx];
+      let uri = token.attrs.uri;
+      return `${`\n      <div class='d-flex justify-content-center mb-3'>\n        <iframe width='560' height='315' src='${uri}' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\n      </div>`}`
     }
   }
 };
--- src/rb/core/md_rules.js.rb
+++ src/rb/core/md_rules.js.rb
@@ -15,13 +15,39 @@ export default class MDRules
     return true
   end
 
+  def self.yt_loader(state, silent)
+    pos = state.pos
+    src = state.src
+
+    match = src.slice(pos).match(/^yt\[(https:\/\/www\.youtube\.com\/embed\/[a-zA-Z0-9_-]+[^\]]*)\]/)
+
+    return false unless match
+    return false if silent
+
+    token = state.push('yt_loader', '', 0)
+    token.attrs = { uri: match[1] }
+
+    state.pos += match[0].length
+    return true
+  end
+
   def self.register(md)
     md.inline.ruler.before('text', 'img_loader', MDRules.img_loader)
-    md.renderer.rules['img_loader'] = lambda do |tokens, idx, options, env, self_obj|
+    md.renderer.rules['img_loader'] = lambda do |tokens, idx|
       token = tokens[idx]
       file_id = token.attrs.file_id
       """<div class='card anim-card border-0 bg-transparent'>
         <elm-img-loader rounded file-id='#{file_id}'></elm-img-loader>
+      </div>"""
+    end
+
+    md.inline.ruler.before('text', 'yt_loader', MDRules.yt_loader)
+    md.renderer.rules['yt_loader'] = lambda do |tokens, idx|
+      token = tokens[idx]
+      uri   = token.attrs.uri
+      """
+      <div class='d-flex justify-content-center mb-3'>
+        <iframe width='560' height='315' src='#{uri}' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>
       </div>"""
     end
   end
